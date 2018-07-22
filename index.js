7589221e4ff382a98a3c3ef54c74c72e1f1e54d7
Daniel James Stephenson <dan_0502@hotmail.co.uk>
2018-07-22 16:53:52 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,6 +3,6 @@ function happyHolidays(string){
   return "Happy holidays!"
 }
 
-function happyHolidaysTo(string){
-  return `Happy holidays, ${you}!`
+function happyHolidaysTo(name){
+  return `Happy holidays, ${name}!`
 }
